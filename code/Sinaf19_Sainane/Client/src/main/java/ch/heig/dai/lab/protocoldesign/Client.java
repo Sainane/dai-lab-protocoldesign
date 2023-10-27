55ea352926c2f2f61b2b3b48abf2239a3c18e65b
Sinaf19 <quentinsurdez@gmail.com>
2023-10-27 12:42:16 +0200

    first try at creating a client

--- code/Sinaf19_Sainane/Client/src/main/java/ch/heig/dai/lab/protocoldesign/Client.java
+++ code/Sinaf19_Sainane/Client/src/main/java/ch/heig/dai/lab/protocoldesign/Client.java
@@ -1,8 +1,13 @@
 package ch.heig.dai.lab.protocoldesign;
 
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
 public class Client {
-    final String SERVER_ADDRESS = "1.2.3.4";
-    final int SERVER_PORT = 1234;
+    final String SERVER_ADDRESS = "localhost";
+    final int SERVER_PORT = 555;
 
     public static void main(String[] args) {
         // Create a new client and run it
@@ -11,5 +16,29 @@ public class Client {
     }
 
     private void run() {
+        try (Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+             BufferedReader in  = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream(),
+                            StandardCharsets.UTF_8));
+
+             Scanner stdin = new Scanner(System.in);
+
+             BufferedWriter out = new BufferedWriter(
+                    new OutputStreamWriter(socket.getOutputStream(),
+                            StandardCharsets.UTF_8))) {
+
+            System.out.println("Please input your calculation: ");
+
+            String calculation = stdin.nextLine() + '\n';
+
+            out.write(calculation);
+            out.flush();
+
+            String result = in.readLine() + '\n';
+            System.out.println(result);
+
+        } catch  (IOException e) {
+            System.out.println("Client: ex.: " + e);
+        }
     }
 }
