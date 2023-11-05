8e6dba85179e2e36879651d822c87c65c2a3d013
Sainane <47831195+Sainane@users.noreply.github.com>
2023-11-05 12:04:34 +0100

    Changes by quentinsurdez

--- code/Sinaf19_Sainane/Client/src/main/java/ch/heig/dai/lab/protocoldesign/Client.java
+++ code/Sinaf19_Sainane/Client/src/main/java/ch/heig/dai/lab/protocoldesign/Client.java
@@ -1,7 +1,12 @@
 package ch.heig.dai.lab.protocoldesign;
 
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
 public class Client {
-    final String SERVER_ADDRESS = "1.2.3.4";
+    final String SERVER_ADDRESS = "127.0.0.1";
     final int SERVER_PORT = 1234;
 
     public static void main(String[] args) {
@@ -11,5 +16,42 @@ public class Client {
     }
 
     private void run() {
+        try (Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+             BufferedReader in  = new BufferedReader(
+                     new InputStreamReader(socket.getInputStream(),
+                             StandardCharsets.UTF_8));
+
+             Scanner stdin = new Scanner(System.in);
+
+             BufferedWriter out = new BufferedWriter(
+                     new OutputStreamWriter(socket.getOutputStream(),
+                             StandardCharsets.UTF_8))) {
+
+
+
+            // System.out.println("Please input your calculation: ");
+
+            // Is it the client that should check for the conformity of the user input ?
+            // or the server ?
+
+            String userInput;
+            Boolean serverCanListen = false;
+            String result;
+            while ((result = in.readLine()) != null) {
+                System.out.println(result);
+
+                if (result.equals("END_OF_OPERATIONS")) {
+                    serverCanListen = true;
+                }
+
+                if (serverCanListen) {
+                    userInput = in.readLine() + "\n";
+                    out.write(userInput);
+                    out.flush();
+                }
+            }
+        } catch  (IOException e) {
+            System.out.println("Client: ex.: " + e);
+        }
     }
 }
